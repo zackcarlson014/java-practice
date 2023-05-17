69be3b55f3053a22acd79d6d433309181c4e8d35
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-17 14:15:49 -0500

    tutorial - variables

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main {
   public static void main(String[] args) {
+    /* The code below will print the words Hello World
+    to the screen */
     System.out.println("Hello World! ");
     System.out.println("I will print on the same line.");
     System.out.println(3);
+    // This code will print out 51 to the screen
     System.out.println(358 / 7);
     System.out.println(50000);
+
+    String name = "John";
+    System.out.println(name);
+
+    int myNum;
+    myNum = 15;
+    System.out.println(myNum);
   }
 }
