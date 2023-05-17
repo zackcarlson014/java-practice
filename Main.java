a309b4019d8e2c1839de83ad7435e8062e963132
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-17 13:42:26 -0500

    tutorial progress

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("I am learning Java.");
-    System.out.println("It is awesome!");
+    System.out.println("Hello World! ");
+    System.out.println("I will print on the same line.");
+    System.out.println(3);
+    System.out.println(358 / 7);
+    System.out.println(50000);
   }
 }
