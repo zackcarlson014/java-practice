cb15360327153ad6493ea9c79e490a9eea8cf36c
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-20 07:04:28 -0500

    multi-dimensional arrays

--- Main.java
+++ Main.java
@@ -265,5 +265,9 @@ public class Main {
     for (int auto = 0; auto < autos.length; auto++) {
       System.out.println(autos[auto]);
     }
+
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+    myNumbers[1][2] = 9;
+    System.out.println(myNumbers[1][2]); // Outputs 9 instead of 7
   }
 }
