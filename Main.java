60035194bcca6e7b79ab0c12332bb9c4a7378c16
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-20 07:50:06 -0500

    if...else return

--- Main.java
+++ Main.java
@@ -16,6 +16,19 @@ public class Main {
     return x + y;
   }
 
+  static void checkAge(int age) {
+
+    // If age is less than 18, print "access denied"
+    if (age < 18) {
+      System.out.println("Access denied - You are not old enough!");
+
+    // If age is greater than, or equal to, 18, print "access granted"
+    } else {
+      System.out.println("Access granted - You are old enough!");
+    }
+
+  }
+
   public static void main(String[] args) {
     /* The code below will print the words Hello World
     to the screen */
@@ -305,5 +318,8 @@ public class Main {
 
     int returnValue = myReturnMethod(4, 5);
     System.out.println(returnValue);
+
+    checkAge(20);
+    checkAge(14);
   }
 }
