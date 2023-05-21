414a2b88277eaad75518e6ff68c3920f0514ecd4
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-21 04:16:11 -0500

    java block scope

--- Main.java
+++ Main.java
@@ -343,5 +343,21 @@ public class Main {
 
     // Code here can use x
     System.out.println(sandwich);
+
+    
+    // Code here CANNOT use x
+
+    { // This is a block
+
+      // Code here CANNOT use x
+
+      int taterTots = 100;
+
+      // Code here CAN use x
+      System.out.println(taterTots);
+
+    } // The block ends here
+
+  // Code here CANNOT use x
   }
 }
