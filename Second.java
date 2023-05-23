ed9a9d53aa23b7e10ef19128225f692a5a22d1b7
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-23 06:51:44 -0500

    class methods across multiple classes

--- Second.java
+++ Second.java
@@ -2,5 +2,9 @@ class Second {
   public static void main(String[] args) {
     Main myObj = new Main();
     System.out.println(myObj.x);
+
+    Main myCar = new Main();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
   }
 }
