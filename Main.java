ab9c79ec5d3bf075a9da184be42b90639f733338
zackcarlson014 <jzcarlson014@gmail.com>
2023-05-23 06:35:07 -0500

    java class attributes

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
   int x = 5;
+  final int finalX = 10;
+
+  String fname = "John";
+  String lname = "Doe";
+  int age = 24;
+
 
   static void myMethod() {
     System.out.println("I just got executed!");
@@ -384,5 +390,21 @@ public class Main {
 
     int haltSum = haltSum(5, 10);
     System.out.println(haltSum);
+
+    Main myObj = new Main();
+    myObj.x = 25; // x is now 25
+    System.out.println(myObj.x);
+
+    // myObj.finalX = 25; // will generate an error: cannot assign a value to a final variable
+    System.out.println(myObj.finalX);
+
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    myObj2.x = 25;
+    System.out.println(myObj1.x);  // Outputs 5
+    System.out.println(myObj2.x);  // Outputs 25
+
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Age: " + myObj.age);
   }
 }
